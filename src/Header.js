f1bb7f4d396c70991470ae899543785ba3394aec
George Michael Brower <george@aaf.nyc>
2019-08-04 11:41:25 -0400

    header

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+export class Header {
+
+    constructor( parent, name ) {
+
+        this.parent = parent;
+
+        this.domElement = document.createElement( 'div' );
+        this.domElement.classList.add( 'header' );
+
+        this.parent.children.push( this );
+        this.parent.$children.appendChild( this.domElement );
+
+        this.name( name );
+
+    }
+
+    name( name ) {
+        this.__name = name;
+        this.domElement.innerHTML = name;
+    }
+
+    destroy() {
+        this.parent.children.splice( this.parent.children.indexOf( this ) );
+        this.parent.$children.removeChild( this.domElement );
+    }
+
+}
