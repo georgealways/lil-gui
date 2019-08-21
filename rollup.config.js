a5f0d7452bff336046657c56b58cf151f537e086
George Michael Brower <george@aaf.nyc>
2019-08-21 11:32:12 -0400

    expose

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import pkg from './package.json';
 import { string } from 'rollup-plugin-string';
 
 export default {
-	input: 'src/GUI.js',
+	input: 'src/main.js',
 	output: [
 		{ file: pkg.module, format: 'es' }
 	],
