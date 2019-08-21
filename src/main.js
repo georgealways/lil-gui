288aa02dcdb3a51ec48338fe5ac3718037021a71
George Michael Brower <george@aaf.nyc>
2019-08-21 11:32:17 -0400

    expose

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// Main class is exposed as both default and named module
+export { GUI as default } from './GUI.js';
+export { GUI } from './GUI.js';
+
+export { Controller } from './Controller.js';
+
+export { BooleanController } from './BooleanController.js';
+export { ColorController } from './ColorController.js';
+export { FunctionController } from './FunctionController.js';
+export { NumberController } from './NumberController.js';
+export { OptionController } from './OptionController.js';
+export { StringController } from './StringController.js';
+
+export { Header } from './Header.js';
