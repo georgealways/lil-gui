0684b8f28a61488a6f197c59a4a3563f1fd2005d
George Michael Brower <george@aaf.nyc>
2019-09-10 18:50:47 -0400

    string controller blur

--- src/StringController.js
+++ src/StringController.js
@@ -10,13 +10,12 @@ export default class StringController extends Controller {
 		this.$input.setAttribute( 'type', 'text' );
 
 		this.$input.addEventListener( 'input', () => {
-			this.setValue( this.$input.value, false );
+			this.setValue( this.$input.value );
 		} );
 
 		this.$input.addEventListener( 'keydown', e => {
 			if ( e.keyCode === 13 ) {
-				this.blur();
-				this._callOnChange();
+				this.$input.blur();
 			}
 		} );
 
