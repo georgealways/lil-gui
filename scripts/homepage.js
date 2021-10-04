b338ca4935c783a926636c9af2f7bed2f9878735
George Michael Brower <george@aaf.nyc>
2021-10-04 09:52:44 -0400

    toc

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -16,7 +16,7 @@ const JSDOC_DEBUG = false;
 
 const md = markdownit( {
 	html: true,
-	highlight: function( code, language ) {
+	highlight: function ( code, language ) {
 		if ( language && hljs.getLanguage( language ) ) {
 			return hljs.highlight( code, { language } ).value;
 		}
@@ -31,13 +31,33 @@ console.log( "jsdocDebug", jsdocDebug );
 
 const template = hbs.compile( read( TEMPLATE ) );
 
+// build TOC from API
 const api = read( API );
-
 let apitoc;
 
-// todo: gross
-api.replace( /# API([\s\S]*)<!--endtoc-->/m, function( a, b ) {
+// this regex catches everything after the first heading, up until a certain comment
+api.replace( /^# API([\s\S]*)<!--endtoc-->/m, function ( a, b ) {
 	apitoc = b.replace( /^#/gm, '##' ); // demote headings
+} );
+
+// build TOC from guide & insert anchors beside guide headers
+let guide = read( GUIDE );
+let guidetoc = '';
+let guidetocIndex = 1;
+const guidePrefix = 'Guide#';
+
+// the regex catches all h2's
+guide = guide.replace( /^## ([\s\S]*?)$/gm, function ( _, heading ) {
+
+	// sanitize title for url
+	const slug = heading.replace( /[^a-zA-Z]+/gm, '-' );
+
+	guidetoc += `${guidetocIndex}. [${heading}](#${guidePrefix}${slug})\n`;
+	guidetocIndex++;
+
+	// inject anchor into guide md
+	return `## <a name="${guidePrefix}${slug}"></a> ${heading}`
+
 } );
 
 const apibody = api.replace( apitoc, '' );
@@ -45,8 +65,9 @@ const apibody = api.replace( apitoc, '' );
 const html = template( {
 	readme: md.render( read( README ) ),
 	apitoc: md.render( apitoc ),
+	guidetoc: md.render( guidetoc ),
 	apibody: md.render( apibody ),
-	guide: md.render( read( GUIDE ) ),
+	guide: md.render( guide ),
 	jsdocDebug,
 	pkg
 } ).replace( new RegExp( `href="${pkg.homepage}`, 'g' ), 'href="' );
