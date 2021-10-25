39cf6bbaeb1483223d4eb22a3f23b76679ac6370
George Michael Brower <george.brower@gmail.com>
2021-10-24 22:20:05 -0400

    boolean hover

--- src/BooleanController.js
+++ src/BooleanController.js
@@ -4,17 +4,12 @@ export default class BooleanController extends Controller {
 
 	constructor( parent, object, property ) {
 
-		super( parent, object, property, 'boolean' );
+		super( parent, object, property, 'boolean', 'label' );
 
 		this.$input = document.createElement( 'input' );
 		this.$input.setAttribute( 'type', 'checkbox' );
 
 		this.$widget.appendChild( this.$input );
-
-		this.$widget.addEventListener( 'click', () => {
-			this.$input.select();
-			this.$input.click();
-		} );
 
 		this.$input.addEventListener( 'change', () => {
 			this.setValue( this.$input.checked );
--- src/Controller.js
+++ src/Controller.js
@@ -5,7 +5,7 @@
  */
 export default class Controller {
 
-	constructor( parent, object, property, className, tagName = 'div' ) {
+	constructor( parent, object, property, className, widgetTag = 'div' ) {
 
 		/**
 		 * The GUI that contains this controller.
@@ -42,7 +42,7 @@ export default class Controller {
 		 * The outermost container DOM element for this controller.
 		 * @type {HTMLElement}
 		 */
-		this.domElement = document.createElement( tagName );
+		this.domElement = document.createElement( 'div' );
 		this.domElement.classList.add( 'controller' );
 		this.domElement.classList.add( className );
 
@@ -60,7 +60,7 @@ export default class Controller {
 		 * The DOM element that contains the controller's "widget" (which differs by controller type).
 		 * @type {HTMLElement}
 		 */
-		this.$widget = document.createElement( 'div' );
+		this.$widget = document.createElement( widgetTag );
 		this.$widget.classList.add( 'widget' );
 
 		this.domElement.appendChild( this.$name );
