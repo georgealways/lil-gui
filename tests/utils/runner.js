14b1cd369d8465787f8b505786657891691c7945
George Michael Brower <george.brower@gmail.com>
2021-11-12 12:12:29 -0500

    allow async tests

--- tests/utils/runner.js
+++ tests/utils/runner.js
@@ -4,7 +4,11 @@ import { AssertionError } from 'assert';
 import fs from 'fs';
 
 // run with --soft-fail to exit with code 0 even if tests don't pass
+
 const SOFT_FAIL = process.argv.includes( '--soft-fail' );
+
+console.log( 'starting tests...' );
+console.time( 'tests' );
 
 // collect tests
 
@@ -25,34 +29,46 @@ fs.readdirSync( 'tests' ).forEach( filename => {
 let failures = 0;
 const numTests = tests.length;
 
-tests.forEach( t => t.run() );
+const promises = tests.map( t => t.run() );
 
 // results
 
-if ( failures > 0 ) {
-	console.log( red( `✕ ${failures}/${numTests} tests failed` ) );
-	process.exit( SOFT_FAIL ? 0 : 1 );
-} else {
-	console.log( grn( `✓ ${numTests}/${numTests} tests passed` ) );
-	process.exit( 0 );
-}
+Promise.all( promises ).then( () => {
+
+	console.timeEnd( 'tests' );
+
+	if ( failures > 0 ) {
+		console.log( red( `✕ ${failures}/${numTests} tests failed` ) );
+		process.exit( SOFT_FAIL ? 0 : 1 );
+	} else {
+		console.log( grn( `✓ ${numTests}/${numTests} tests passed` ) );
+		process.exit( 0 );
+	}
+
+} );
 
 function Test( name, test ) {
 	this.name = name;
 	this.run = () => {
 		try {
-			test();
+			const promise = test();
+			// todo: errors in async tests aren't making it to catchError?
+			if ( promise ) promise.catch( catchError );
+			return promise;
 		} catch ( e ) {
-			failures++;
-			if ( e instanceof AssertionError ) {
-				console.log( red( `✕ ${e.message}` ) );
-				console.log( e.stack );
-			} else {
-				console.log( red( `✕ Unexpected error in test: ${this.name}` ) );
-				console.log( e.stack );
-			}
+			catchError( e );
 		}
 	};
+	function catchError( e ) {
+		failures++;
+		if ( e instanceof AssertionError ) {
+			console.log( red( `✕ ${e.message}` ) );
+			console.log( e.stack );
+		} else {
+			console.log( red( `✕ Unexpected error in test: ${this.name}` ) );
+			console.log( e.stack );
+		}
+	}
 }
 
 function red( str ) { return `\x1b[31m${str}\x1b[0m`; }
