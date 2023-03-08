0c7491babae0afcaff9d24bb6c221bcf7a852ae5
George Michael Brower <george.brower@gmail.com>
2023-03-08 17:21:27 -0500

    test runner cleanup

--- tests/utils/runner.js
+++ tests/utils/runner.js
@@ -1,36 +1,29 @@
 import './shim.js';
-
-import { AssertionError } from 'assert';
 import fs from 'fs';
 
 // run with --soft-fail to exit with code 0 even if tests don't pass
-
 const SOFT_FAIL = process.argv.includes( '--soft-fail' );
 
 console.time( 'tests' );
 
-// collect tests
-
-const tests = [];
-const testFiles = fs.readdirSync( 'tests' );
-
-for ( const filename of testFiles ) {
-	if ( !filename.endsWith( '.js' ) ) continue;
-	const test = await import( '../' + filename );
-	const name = filename.replace( '.js', '' );
-	tests.push( new Test( name, test.default ) );
-}
-
 // run tests
 
 let failures = 0;
-const numTests = tests.length;
 
-tests.forEach( t => t.run() );
+const tests = fs.readdirSync( 'tests' )
+	.filter( file => file.endsWith( '.js' ) )
+	.map( async file => {
+		const module = await import( '../' + file );
+		run( file, module.default );
+	} );
+
+await Promise.all( tests );
 
 // results
 
 console.timeEnd( 'tests' );
+
+const numTests = tests.length;
 
 if ( failures > 0 ) {
 	console.log( red( `✕ ${failures}/${numTests} tests failed` ) );
@@ -40,22 +33,14 @@ if ( failures > 0 ) {
 	process.exit( 0 );
 }
 
-function Test( name, test ) {
-	this.name = name;
-	this.run = () => {
-		try {
-			test();
-		} catch ( e ) {
-			failures++;
-			if ( e instanceof AssertionError ) {
-				console.log( red( `✕ ${e.message}` ) );
-				console.log( e.stack );
-			} else {
-				console.log( red( `✕ Unexpected error in test: ${this.name}` ) );
-				console.log( e.stack );
-			}
-		}
-	};
+function run( name, test ) {
+	try {
+		test();
+	} catch ( e ) {
+		failures++;
+		console.log( red( `Error in test: ${name}` ) );
+		console.log( e.stack );
+	}
 }
 
 function red( str ) { return `\x1b[31m${str}\x1b[0m`; }
