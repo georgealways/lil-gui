8dbc44a67a7fece85ae5aabdf0ac2d822d6356e9
George Michael Brower <george.brower@gmail.com>
2023-04-07 08:26:53 -0400

    folders inherit disabled touch styles

--- tests/utils/shim.js
+++ tests/utils/shim.js
@@ -35,7 +35,13 @@ class EventTarget {
 class Element extends EventTarget {
 	constructor() {
 		super();
-		this.classList = { add() {}, remove() {}, toggle() {} };
+		const classes = new Set();
+		this.classList = {
+			add: c => classes.add( c ),
+			contains: c => classes.has( c ),
+			remove() {},
+			toggle() {}
+		};
 		this.style = { setProperty() {} };
 		this.parentElement = { removeChild() {} };
 	}
