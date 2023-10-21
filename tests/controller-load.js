596d3e0f0a84974230e216568d3b188c60fb2cec
George Michael Brower <george.brower@gmail.com>
2023-10-21 10:51:40 -0400

    controller-load remove inner declarations

--- tests/controller-load.js
+++ tests/controller-load.js
@@ -9,36 +9,32 @@ export default () => {
 
 	const gui = new GUI();
 
-	{
-		const tracker = new CallTracker();
-		const ctrl = gui.add( { x: 0 }, 'x' ).onFinishChange( tracker.handler );
-		assert.strictEqual( ctrl.load( 90 ), ctrl, 'load returns this' );
-		assert.strictEqual( tracker.numCalls, 1 );
-	}
-	{
-		const tracker = new CallTracker();
-		const ctrl = gui.add( { x: false }, 'x' ).onFinishChange( tracker.handler );
-		assert.strictEqual( ctrl.load( true ), ctrl, 'load returns this' );
-		assert.strictEqual( tracker.numCalls, 1 );
-	}
-	{
-		const tracker = new CallTracker();
-		const ctrl = gui.add( { x: 'foo' }, 'x' ).onFinishChange( tracker.handler );
-		assert.strictEqual( ctrl.load( 'bar' ), ctrl, 'load returns this' );
-		assert.strictEqual( tracker.numCalls, 1 );
-	}
-	{
-		const tracker = new CallTracker();
-		const ctrl = gui.add( { x: 'foo' }, 'x', [ 'bar', 'baz' ] ).onFinishChange( tracker.handler );
-		assert.strictEqual( ctrl.load( 'bar' ), ctrl, 'load returns this' );
-		assert.strictEqual( tracker.numCalls, 1 );
-	}
-	{
-		const tracker = new CallTracker();
-		const obj = { x: [ 0, 1, 0 ] };
-		const ctrl = gui.addColor( obj, 'x' ).onFinishChange( tracker.handler );
-		assert.strictEqual( ctrl.load( '#ff00ff' ), ctrl, 'load returns this' );
-		assert.strictEqual( tracker.numCalls, 1 );
-	}
+	let tracker, ctrl;
+
+	tracker = new CallTracker();
+	ctrl = gui.add( { x: 0 }, 'x' ).onFinishChange( tracker.handler );
+	assert.strictEqual( ctrl.load( 90 ), ctrl, 'load returns this' );
+	assert.strictEqual( tracker.numCalls, 1 );
+
+	tracker = new CallTracker();
+	ctrl = gui.add( { x: false }, 'x' ).onFinishChange( tracker.handler );
+	assert.strictEqual( ctrl.load( true ), ctrl, 'load returns this' );
+	assert.strictEqual( tracker.numCalls, 1 );
+
+	tracker = new CallTracker();
+	ctrl = gui.add( { x: 'foo' }, 'x' ).onFinishChange( tracker.handler );
+	assert.strictEqual( ctrl.load( 'bar' ), ctrl, 'load returns this' );
+	assert.strictEqual( tracker.numCalls, 1 );
+
+	tracker = new CallTracker();
+	ctrl = gui.add( { x: 'foo' }, 'x', [ 'bar', 'baz' ] ).onFinishChange( tracker.handler );
+	assert.strictEqual( ctrl.load( 'bar' ), ctrl, 'load returns this' );
+	assert.strictEqual( tracker.numCalls, 1 );
+
+	tracker = new CallTracker();
+	const obj = { x: [ 0, 1, 0 ] };
+	ctrl = gui.addColor( obj, 'x' ).onFinishChange( tracker.handler );
+	assert.strictEqual( ctrl.load( '#ff00ff' ), ctrl, 'load returns this' );
+	assert.strictEqual( tracker.numCalls, 1 );
 
 };
