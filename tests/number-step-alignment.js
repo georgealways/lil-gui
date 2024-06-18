ed6627434539085fabbcc243d7570daa1e50e433
George Michael Brower <george.brower@gmail.com>
2024-06-17 20:23:53 -0400

    more test cases

--- tests/number-step-alignment.js
+++ tests/number-step-alignment.js
@@ -3,34 +3,67 @@ import { GUI } from '../dist/lil-gui.esm.min.js';
 
 export default () => {
 
-	const gui = new GUI();
-
-	const target = { x: 0 };
-	const controller = gui.add( target, 'x', 1, 11 ).step( 2 );
-
-	const expectedValues = new Set( [ 1, 3, 5, 7, 9, 11 ] );
-	const actualValues = new Set();
-
-	const rect = controller.$slider.getBoundingClientRect();
-
-	assert.strictEqual( target.x, 0, "step/min/max don't affect value until user interaction" );
-
-	controller.$slider.$callEventListener( 'mousedown', {
-		clientX: Math.floor( rect.left )
+	stepTest( {
+		start: 0,
+		min: 1,
+		max: 10,
+		step: 1,
+		expectedValues: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 	} );
 
-	// move the mouse across the slider
-	for ( let clientX = Math.floor( rect.left ); clientX < Math.ceil( rect.right ); clientX++ ) {
-		window.$callEventListener( 'mousemove', { clientX } );
-		actualValues.add( target.x );
+	stepTest( {
+		start: 0,
+		min: 1,
+		max: 11,
+		step: 2,
+		expectedValues: [ 3, 5, 7, 9, 11 ]
+	} );
+
+	stepTest( {
+		start: 0,
+		min: 10,
+		step: 3,
+		expectedValues: [ 10, 13, 16, 19, 22, 25, 28 ]
+	} );
+
+	stepTest( {
+		start: 21,
+		max: 20,
+		step: 7,
+		expectedValues: [ 13, 6, -1, -8, -15, -22 ]
+	} );
+
+	function stepTest( { start, min, max, step, expectedValues } ) {
+
+		const gui = new GUI();
+
+		const target = { x: start };
+		const controller = gui.add( target, 'x', min, max, step );
+
+		const actualValues = new Set();
+
+		assert.strictEqual( target.x, start, "value isn't modified until user interaction." );
+
+		for ( let i = 0; i < expectedValues.length; i++ ) {
+
+			// key up if min is defined, otherwise key down
+			const code = min !== undefined ? 'ArrowUp' : 'ArrowDown';
+
+			controller.$input.$callEventListener( 'keydown', { code } );
+			actualValues.add( target.x );
+
+		}
+
+		window.$callEventListener( 'mouseup' );
+
+		assert.deepStrictEqual(
+			actualValues,
+			new Set( expectedValues ),
+			'slider steps correctly even when min/max are not divisible by step. ' +
+			`actual: ${[ ...actualValues ]} ` +
+			`expected: ${[ ...expectedValues ]}`
+		);
+
 	}
-
-	window.$callEventListener( 'mouseup' );
-
-	assert.deepStrictEqual(
-		actualValues,
-		expectedValues,
-		'slider steps correctly even when min/max are not divisible by step'
-	);
 
 };
