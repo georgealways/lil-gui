974955ca9fcfcb120f44e8147eef23313b9312af
George Michael Brower <george.brower@gmail.com>
2024-10-20 13:50:38 -0400

    better logging for types task

--- scripts/types.js
+++ scripts/types.js
@@ -2,31 +2,51 @@ import fs from 'fs';
 
 const TYPES_FILE = 'dist/lil-gui.esm.d.ts';
 
-const OLD_PARAMS = '\\(object: object, property: string';
+let contents = fs.readFileSync( TYPES_FILE, 'utf-8' );
+
+const OLD_PARAMS = '(object: object, property: string';
 const NEW_PARAMS = '<T>(object: T, property: keyof T';
 
-let contents = fs.readFileSync( TYPES_FILE, 'utf8' );
+console.log( 'Updating types file...\n' );
 
-const replace = ( before, after ) => contents.replace( new RegExp( before, 'g' ), after );
-
-contents = replace(
+replace(
 	`add${OLD_PARAMS}`,
 	`add${NEW_PARAMS}`
 );
 
-contents = replace(
+replace(
 	`addColor${OLD_PARAMS}`,
 	`addColor${NEW_PARAMS}`
 );
 
-contents = replace(
-	'\\* @param \\{object\\} object',
+replace(
+	'* @param {object} object',
 	'* @param {T} object'
 );
 
-contents = replace(
-	'\\* @param \\{string\\} property',
+replace(
+	'* @param {string} property',
 	'* @param {keyof T} property'
 );
 
 fs.writeFileSync( TYPES_FILE, contents );
+
+function replace( before, after ) {
+	const regex = new RegExp( escapeRegExp( before ), 'g' );
+	const matches = contents.match( regex );
+	const count = matches ? matches.length : 0;
+	if ( count === 0 ) {
+		console.log( 'Error: Could not find any instances of:\n' );
+		console.log( `  ${before}\n` );
+		process.exit( 1 );
+	} else {
+		console.log( `Replaced ${count} instances:` );
+		console.log( `-   ${before}` );
+		console.log( `+   ${after}\n` );
+		contents = contents.replace( regex, after );
+	}
+}
+
+function escapeRegExp( string ) {
+	return string.replace( /[.*+?^${}()|[\]\\]/g, '\\$&' );
+}
